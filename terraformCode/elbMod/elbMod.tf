d25a80b12d2c6e34c666c703b3dbc5dee2e31cca
segityun <advtyun@gmail.com>
2021-03-05 12:28:54 +0200

    created new security groups and added the inbound and outbout rules with the relevant IPs

--- terraformCode/elbMod/elbMod.tf
+++ terraformCode/elbMod/elbMod.tf
@@ -7,17 +7,25 @@ variable "nginxPetOne" {
 variable "nginxPetTwo" {
   type = string
 }
+variable "segment_public" {
+  type = string
+}
+variable "segment_private" {
+  type = string
+}
 
-# Create a new load balancer
+# Create a new Public load balancer
 resource "aws_elb" "petClinicPublicELB" {
   name               = "petClinicPublicELB"
   availability_zones = ["us-east-2"]
   internal = false
   load_balancer_type = "application"
   security_groups = [ var.security_group_id ]
+  subnets = [ var.segment_public ]
+      
 
   listener {
-    instance_port     = 8000
+    instance_port     = 80
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
@@ -39,5 +47,39 @@ resource "aws_elb" "petClinicPublicELB" {
 
   tags = {
     Name = "petClinicPublicELB"
+  }
+}
+# Create a new Private load balancer
+resource "aws_elb" "petClinicPrivateELB" {
+  name               = "petClinicPrivateELB"
+  availability_zones = ["us-east-2"]
+  internal = true
+  load_balancer_type = "application"
+  security_groups = [ var.security_group_id ]
+  subnets = [ var.segment_private ]
+
+  listener {
+    instance_port     = 8080
+    instance_protocol = "http"
+    lb_port           = 8080
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  instances                   = [var.nginxPetTwo, var.nginxPetOne]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "petClinicPrivateELB"
   }
 }
--- terraformCode/secGrp/secGrp.tf
+++ terraformCode/secGrp/secGrp.tf
@@ -10,13 +10,14 @@ output "security_group_id" {
 # use ingresses protocols as variables
 variable "ingresses" {
   type    = list(number)
-  default = [8080, 443, 22, 3306]
+  default = [80]
 }
 
-# create aws security group with MYSQL,HTTPS,SSH inbound rules
-resource "aws_security_group" "secGrpPets" {
-  name        = "Connections"
-  description = "Allow traffic between all of the components in our network"
+##### First security Group
+# create aws security group with http inbound rules
+resource "aws_security_group" "secGrpNginx" {
+  name        = "secGrpNginx"
+  description = "Allow traffic between users to our nginx servers"
   vpc_id      = var.vpc_id
 
 # add dynamic ingress rules list
@@ -39,6 +40,66 @@ resource "aws_security_group" "secGrpPets" {
   }
 
   tags = {
-    "secGrpPets" = "true"
+    "secGrpNginx" = "true"
+  }
+}
+##### Second security Group
+# create aws security group with http inbound rules
+resource "aws_security_group" "secGrpApp" {
+  name        = "secGrpApp"
+  description = "Allow traffic between nginx VM to App VM"
+  vpc_id      = var.vpc_id
+
+# add dynamic ingress rules list
+ dynamic "ingress" {
+    iterator = port
+    for_each = var.ingresses
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["10.0.1.0/24"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "secGrpApp" = "true"
+  }
+}
+##### Third security Group
+# create aws security group with http inbound rules
+resource "aws_security_group" "secGrpRDS" {
+  name        = "secGrpRDS"
+  description = "Allow traffic between nginx VM to App VM"
+  vpc_id      = var.vpc_id
+
+# add dynamic ingress rules list
+ dynamic "ingress" {
+    iterator = port
+    for_each = var.ingresses
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["10.0.2.0/24"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "secGrpRDS" = "true"
   }
 }
