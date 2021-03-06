61dbcded48bf798e8dc518130ce3820cdaa89906
segityun <yossizxc@gmail.com>
2021-03-06 17:39:25 +0200

    changed app.sh to work properly

--- terraformCode/appSrv/app.sh
+++ terraformCode/appSrv/app.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install default-jdk
-sudo apt install docker.io -y
-sudo git clone git@github.com:spring-projects/spring-petclinic.git
+sudo apt install default-jdk -y
+sudo git clone https://github.com/spring-projects/spring-petclinic
 cd spring-petclinic
 ./mvnw spring-boot:run
