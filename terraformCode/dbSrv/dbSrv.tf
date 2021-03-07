c926dbacc1901bd4b5ce6f7dfec14c875f66b818
yossizxc <yossizxc@gmail.com>
2021-03-07 20:28:33 +0200

    s

--- terraformCode/dbSrv/dbSrv.tf
+++ terraformCode/dbSrv/dbSrv.tf
@@ -5,13 +5,8 @@ resource "aws_db_instance" "petClinicDB" {
   engine_version       = "8.0.20"
   instance_class       = "db.t2.micro"
   name                 = "petClinicDB"
-<<<<<<< HEAD
   username             = var.db_username
   password             = var.db_password
-=======
-  username             =  var.db_username
-  password             =  var.db_username
->>>>>>> 8b1f36d5248709436b602d27699336d252ccee89
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
